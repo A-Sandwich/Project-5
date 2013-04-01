d02c84740330dafd49659c07da82b785e49cb6d9
Kyle Burkholder <kbburkholder@gmail.com>
2013-04-01 18:00:14 -0400

    Base
    This is the base to build project 5 off of. It is just project 4 without
    any changes. It will use google maps to allow for the deletion of
    markers and searching within a radius.

---
+++ assets/js/app.js
@@ -0,0 +1,148 @@
+;(function($, window) {
+	
+	var jQT;
+	
+    $(function(){
+	
+	    jQT = new $.jQTouch({
+	        statusBar: 'black-translucent',
+	        preloadImages: []
+	    });
+
+    });
+    
+	$('#home').bind('pageAnimationEnd', function(event, info) {
+		if (info.direction == 'in') {
+			$("#map").show();
+			
+			google.maps.event.trigger(map.map, 'resize');
+			
+			map.map.setZoom(map.mapOptions.zoom);
+			map.map.fitBounds(map.bounds);
+					
+		}
+		return false;
+	});
+
+	var map = $('#map').MobileMap({
+		mapOptions: {
+			center: new google.maps.LatLng(39.76, -86.15)//Coordinates = Indianapolis
+		},
+		callback: {
+			newMarker: function(marker, lat, lng, index) {
+				google.maps.event.addListener(marker, 'click', function() {
+					
+					map.editIndex = index;
+					
+					var row     = map.db.query('markers', function(row) {
+						if(row.ID == index+1) {
+							return true;
+						}
+						return false;
+					});
+					
+					row = row[0];
+					
+					var $name   = $('#editLoc').find('#locationName');
+					var $street = $('#editLoc').find('#streetAddress');
+					var $city   = $('#editLoc').find('#city');
+					var $state  = $('#editLoc').find('#state');
+					var $zip    = $('#editLoc').find('#zipCode');
+									
+					$name.val(row.name);
+					$street.val(row.street);
+					$city.val(row.city);
+					$state.val(row.state);	
+					$zip.val(row.zipcode);
+					
+					jQT.goTo('#updateLocation', 'slideup');		
+					
+				});
+			}
+		}
+	});
+	
+	$('#newLoc').submit(function(e) {
+		var $thisLocation      = $(this);
+		var $name   = $thisLocation.find('#locationName');
+		var $street = $thisLocation.find('#streetAddress');
+		var $city   = $thisLocation.find('#city');
+		var $state  = $thisLocation.find('#state');
+		var $zip    = $thisLocation.find('#zipCode');
+		
+		var address = [
+			$street.val(),
+			$city.val(),
+			$state.val(),
+			$zip.val()
+		];
+		
+		var object = {
+			name: $name.val(),
+			address: address.join(' '),
+			street: $street.val(),
+			city: $city.val(),
+			state: $state.val(),
+			zipcode: $zip.val()
+		}
+		
+		//Send information to mobileMap to be used
+		map.addMarker(object, function() {
+			map.home();
+			$name.val('');
+			$street.val('');
+			$city.val('');
+			$state.val('');
+			$zip.val('');
+		});
+		
+		e.preventDefault();
+		
+		window.getElementById('#home').click();
+		//window.location = "#home";
+		
+		return false;
+	});
+	
+	$('#editLoc').submit(function(e) {
+		
+		var $thisLocation      = $(this);
+		var $name   = $thisLocation.find('#locationName');
+		var $street = $thisLocation.find('#streetAddress');
+		var $city   = $thisLocation.find('#city');
+		var $state  = $thisLocation.find('#state');
+		var $zip    = $thisLocation.find('#zipCode');
+		
+		var address = [
+			$street.val(),
+			$city.val(),
+			$state.val(),
+			$zip.val()
+		];
+		
+		var object = {
+			name: $name.val(),
+			address: address.join(' '),
+			street: $street.val(),
+			city: $city.val(),
+			state: $state.val(),
+			zipcode: $zip.val()
+		}
+		
+		map.editMarker(object, function() {
+			map.home();
+			$name.val('');
+			$street.val('');
+			$city.val('');
+			$state.val('');
+			$zip.val('');
+		});
+		
+		e.preventDefault();
+	
+		window.location = "#home";
+		
+		return false;
+	});
+	
+}(jQuery, this));
---
+++ assets/js/mobileMap.js
@@ -0,0 +1,258 @@
+;(function($) {
+	
+	var MobileMap = function(object, options) {
+	
+		var $thisMap = $(object);
+		
+		var thisMap = {
+			
+			callback: {
+				newMarker: function(marker, lat, lng) {},	//calls new marker
+			},
+			db: new localStorageDB("MapIndex", localStorage),//storage variable
+			bounds: new google.maps.LatLngBounds(),
+			editIndex: false,
+			geocoder: new google.maps.Geocoder(),
+			map: false,
+			mapOptions: {
+				zoom: 14,
+				center: new google.maps.LatLng(0, 0), 
+				mapTypeId: google.maps.MapTypeId.SATELLITE, 
+				panControl: false,//disables pan control!
+				scrollwheel: false, // Doesn't allow zoom via scroll wheel on mouse, does not disable zoom scrool wheel on side.
+				//navigationControl: false, gets rid of ui
+				//mapTypeControl: false, gets rid of map type button
+			},
+			markers: [],
+			ui: {
+				map: $thisMap
+			}
+			
+		}
+		
+		if(!options) {
+			var options = {};
+		}
+		
+		thisMap = $.extend(true, thisMap, options);
+		
+		thisMap.initialize = function(options) {
+			
+			if(options) {
+				thisMap.mapOptions = $.extend(true, thisMap.mapOptions, options);	
+			}
+			
+			thisMap.map = new google.maps.Map(thisMap.ui.map.get(0), thisMap.mapOptions);
+			
+			if(!thisMap.db.tableExists('markers')) {	//If a table does not exist then:		
+			    thisMap.db.createTable("markers", ["name", "address", "response", "street", "city", "state", "zipcode", "lat", "lng"]);
+			    thisMap.db.commit();
+			}
+			
+			thisMap.db.query('markers', function(row) {//Add new marker to table (That exists)
+				thisMap.newMarker(row.lat, row.lng);
+			});
+			
+			return thisMap.map;
+		}
+		
+		thisMap.home = function() {
+			
+			google.maps.event.trigger(thisMap.map, 'resize');
+			thisMap.map.setZoom(thisMap.mapOptions.zoom);
+			thisMap.map.fitBounds(thisMap.bounds);
+			
+			$('a[href="#home"]').click();
+		}
+		
+		
+		//Adds a new marker
+		thisMap.newMarker = function(lat, lng) {
+			var latLng = new google.maps.LatLng(lat, lng);
+		
+			marker = new google.maps.Marker({
+				map: thisMap.map,
+				position: latLng 
+			});
+			
+			thisMap.callback.newMarker(marker, lat, lng, thisMap.markers.length);
+			
+			thisMap.markers.push(marker);//Puts marker on map
+			thisMap.bounds.extend(latLng);//extends bounds of map to new point.
+			thisMap.map.fitBounds(thisMap.bounds);//Adjusts map's viewport
+			
+			return marker;
+		}
+		
+		thisMap.addMarker = function(location, save, callback) {
+			
+			if(typeof save == "undefined") {
+				var save = true;
+			}
+			
+			if(typeof save == "function") {
+				callback = save;
+				save = true;
+			}
+			
+			//Is called by app.js and given search info
+			thisMap.geocode(location.address, function(response) {
+			
+				if(response.success) {
+					var lat = response.results[0].geometry.location.lat();
+					var lng = response.results[0].geometry.location.lng();
+					var hasLatLng = thisMap.hasLatLng(lat, lng);
+					var marker = false;
+					
+					if(hasLatLng) {
+						alert('This location has already been entered');	
+					}
+					else {						
+						thisMap.newMarker(lat, lng);
+						
+						if(typeof callback == "function") {
+							callback(response, location, save);
+						}
+					}
+					
+					if(save && !hasLatLng) {
+						thisMap.db.insert("markers", {
+							name: location.name,
+							address: location.address,
+							street: location.street,
+							city: location.city,
+							state: location.state,
+							zipcode: location.zipcode,
+							response: response,
+							lat: lat,
+							lng: lng
+						});
+						
+						thisMap.db.commit();
+					}
+				}
+				else {
+					alert('This is not a location.');
+				}
+				
+				thisMap.geocode = function(location, callback) {
+					if(typeof callback != "function") {
+					callback = function() {};
+				}
+			
+				thisMap.geocoder.geocode({'address': location}, function(results, status) {
+				
+					var response = {
+						success: status == google.maps.GeocoderStatus.OK ? true : false,
+						status: status,
+						results: results
+					}
+				
+					callback(response);
+				});
+				thisMap.fitBounds(thisMap.bounds);
+			}
+			});
+			;
+		}
+		
+		
+		
+		//Checks to see if lat&&lng are already in table
+		thisMap.hasLatLng = function(lat, lng) {
+			var _return = false;
+			
+			thisMap.db.query('markers', function(row) {
+				if(row.lat == lat && row.lng == lng) {
+					_return = true;	
+				}
+			});
+			
+			return _return;
+		}
+		
+		thisMap.updateMarker = function(marker, lat, lng) {
+			marker.setPosition(new google.maps.LatLng(lat, lng));
+		}
+		
+		thisMap.editMarker = function(location, callback) {
+			
+			thisMap.geocode(location.address, function(response) {
+				if(response.success) {
+					
+					var lat = response.results[0].geometry.location.lat();
+					var lng = response.results[0].geometry.location.lng();
+					var hasLatLng = thisMap.hasLatLng(lat, lng);
+					
+					if(hasLatLng) {
+						alert('\''+$.trim(location.address)+'\' is already a location on the map');	
+					}
+					else {						
+						thisMap.updateMarker(thisMap.markers[thisMap.editIndex], lat, lng);
+									
+						thisMap.db.update("markers", {ID: thisMap.editIndex+1}, function() {
+							var row = {
+								name: location.name,
+								address: location.address,
+								street: location.street,
+								city: location.city,
+								state: location.state,
+								zipcode: location.zipcode,
+								response: response,
+								lat: lat,
+								lng: lng
+							}
+							
+						console.log(row);
+						
+							return row;
+						});
+						
+						thisMap.db.commit();
+						
+						if(typeof callback == "function") {
+							callback(response, location);
+						}
+						
+						
+					}
+				}
+				else {
+					alert('This is an invalid location');
+				}
+			});
+			
+		}
+		
+		thisMap.resize = function(){
+			thisMap.bounds.extend(latLng);//extends bounds of map to new point.
+			thisMap.map.fitBounds(thisMap.bounds)
+		}
+		
+		thisMap.geocode = function(location, callback) {
+			if(typeof callback != "function") {
+				callback = function() {};
+			}
+			
+			thisMap.geocoder.geocode({'address': location}, function(results, status) {
+				
+				var response = {
+					success: status == google.maps.GeocoderStatus.OK ? true : false,
+					status: status,
+					results: results
+				}
+				
+				callback(response);
+			});
+		}
+		
+		thisMap.initialize();
+		
+		return thisMap;
+	}
+	
+	$.fn.MobileMap = function(options) {
+		return new MobileMap($(this), options);
+	}	
+	
+})(jQuery);
